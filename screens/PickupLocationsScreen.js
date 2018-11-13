f3b26a983ce82cc7669014eaed3424d77099ddb6
Emz27 <emmanuelbenitez1996@gmail.com>
2018-11-14 03:05:49 +0800

    layout reworked

--- screens/PickupLocationsScreen.js
+++ screens/PickupLocationsScreen.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { Button, TouchableWithoutFeedback, Text, View, AsyncStorage, FlatList, StyleSheet, Animated, Dimensions } from 'react-native'
+import { DrawerLayoutAndroid, Button, TouchableWithoutFeedback, Text, View, AsyncStorage, FlatList, StyleSheet, Animated, Dimensions } from 'react-native'
+import { MapView } from 'expo'
 import { ExpoConfigView } from '@expo/samples';
+
 
 import * as firebase from 'firebase';
 import 'firebase/firestore';
 
+
 let { width: windowX } = Dimensions.get('window')
+
+let { width, height } = Dimensions.get('window')
+const ASPECT_RATIO = width / height
+const LATITUDE_DELTA = 0.008
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO
 
 export default class PickupLocationsScreen extends React.Component {
   constructor(props){
@@ -19,8 +27,8 @@ export default class PickupLocationsScreen extends React.Component {
     
   }
   static navigationOptions = {
-    title: 'Pickup Locations',
-  };
+    header: null,
+  }
   onPressSignOut = async () =>{
     await AsyncStorage.removeItem('eBasuraNavigationUser');
     this.props.navigation.navigate('AuthLoading');
@@ -58,40 +66,25 @@ export default class PickupLocationsScreen extends React.Component {
     return (
       <View style={styles.container}>
 
-      
-      <FlatList contentContainerStyle={styles.list}
-        data={this.state.pickuplocations}
-        renderItem={({item}) => 
-            <View>
-              <TouchableWithoutFeedback onPress={()=>{
-                  console.log("preesss");
-                  Animated.parallel(
-                    [
-                      Animated.spring( item.itemWidth, {toValue: (item.isLarge)?item.itemWidthMin:item.itemWidthMax,}),
-                    ],
-                    {
-                      useNativeDriver: true
-                    }
-                  ).start();
-                  item.isLarge = !item.isLarge;
-                }
-              }>
-              <Animated.View style={[{transform:[{ translateX: item.itemWidth}]}, styles.listItem ]}>
+        <View style={styles.mapContainer}>
+          <MapView
+            style={styles.map}
+            initialRegion={{
+              latitude: 14.61881,
+              longitude: 121.057171,
+              latitudeDelta: LATITUDE_DELTA,
+              longitudeDelta: LONGITUDE_DELTA,
+          }}>
 
-                  <View style={{flex:1}}><Text>{item.address}</Text></View>
-                  <View><Text>{item.pickupid}</Text></View>
+          </MapView>
+        </View>
 
-              </Animated.View>
-            </TouchableWithoutFeedback>
-            <View style={{position:"absolute",right:0,zIndex:-1000, width:(-1)*windowX*.8,flex:1, flexDirection:"row-reverse"}}>
-              <View style={styles.itemButton}></View>
-              <View style={styles.itemButton}></View>
-              <View style={styles.itemButton}></View>
-            </View>
-            </View>
-            
-        }
-      />
+        <FlatList contentContainerStyle={styles.listContainer}
+          data={this.state.pickuplocations}
+          renderItem={({item}) => <View></View>
+          }
+        />
+
       </View>
     );
   }
@@ -102,15 +95,14 @@ const styles = StyleSheet.create({
   container:{
     flex:1
   },
-  list :{
-    flex:1,
-    alignItems:"flex-start",
+  mapContainer:{
+    flex:1
   },
-  listItemContainer:{
-    padding: 20,
-    marginTop:5,
-    backgroundColor: "#01ffbf",
-    marginHorizontal:"auto",
+  map:{
+    flex:1
+  },
+  listContainer :{
+    flex:3,
   },
   listItem:{
     backgroundColor: "#02ffe5",
@@ -124,14 +116,5 @@ const styles = StyleSheet.create({
     width: windowX*.9,
     overflow:"hidden",
     position:"relative",
-  },
-  itemButton:{
-    width:15,
-    height:15,
-    marginVertical:"auto",
-    borderRadius: 15, 
-    elevation: 10,
-    marginRight: 20, 
-    backgroundColor:"#01ffbf"
   }
 });
