4c2baf4c94dab4cbc9de0a3f8ddc88b618b75eeb
Emmanuel Rico Benitez <ebenitez@ph-wwos.com>
2018-11-16 18:55:24 +0800

    misc

---
+++ dbmodel.js
@@ -0,0 +1,55 @@
+
+const collection = {
+    address: "Socorro, Cubao, Quezon City, Metro Manila",
+    comment: "",
+    dateTime: "November 20, 2018 at 12:00:00 AM UTC+8",
+    location: "[14.61881° N, 121.057171° E]",
+    pickupId: "A1B1",
+    pickupDocId: "/pickup locations/9Si8P1N2YutaM46NWTa5",
+    status: "pending",
+    truckId:"",
+    truckDocId: "",
+    collectors: [],
+}
+
+const user = {
+    userId: "",
+    password: "",
+    truckId: "",
+    type: "",
+}
+
+const pickupLocation = {
+    pickupId: "",
+    address: "",
+    comment: "",
+    location: {
+        latitude:"",
+        longitude: "",
+    },
+    route:{
+        
+    }
+}
+
+const truck = {
+    truckId:"",
+    collectors:[],
+    location: {
+        latitude:"",
+        longitude: "",
+    },
+    route: {
+
+    }
+}
+
+
+
+
+export default {
+    collection,
+    user,
+    truck,
+    pickupLocation
+};
---
+++ firebaseconfig.js
@@ -0,0 +1,6 @@
+
+
+export default {
+    apiKey: 'AIzaSyCHOMBr5-qyYELVws4Exa6hO8V-utuBl58',
+    projectId: 'bamboo-rhino-221107'
+}
